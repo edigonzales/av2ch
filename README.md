b26e766080b7cdc296c264d4fde25fe1b2eb0a81
Stefan Ziegler <edi.gonzales@gmail.com>
2019-03-11 19:52:29 +0100

    convert method and first test

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
+[![Build Status](https://travis-ci.org/sogis/av2ch.svg?branch=master)](https://travis-ci.org/sogis/av2ch)
 # av2ch
-Convert a cadastral INTERLIS dataset (itf) from the cantonal model to the federal model.
+Converts a cadastral INTERLIS dataset (itf) from the cantonal model to the federal model.
+
+It supports german and italian transfer files. Only LV95 is supported. Originally written for the canton of Solothurn, it actually should work for canton of Glarus and Tessin.
+
+```
+./gradle clean build
+```
+
+Travis deploys library to bintray/jcenter.
+
+Library is used in GRETL.
 
 Tasks:
 - Javadoc
@@ -8,3 +19,4 @@ Tasks:
 - Transfer ownership
 - Deploy library to jcenter
 - GRETL-Task
+- README
--- src/test/java/ch/so/agi/av/Av2chTest.java
+++ src/test/java/ch/so/agi/av/Av2chTest.java
@@ -1,7 +1,32 @@
 package ch.so.agi.av;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.nio.file.Path;
+
+import org.interlis2.validator.Validator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import ch.ehi.basics.settings.Settings;
+import ch.interlis.ili2c.Ili2cException;
+import ch.interlis.iox.IoxException;
+
 public class Av2chTest {
-    public void testSomeLibraryMethod() {
-//        Library classUnderTest = new Library();
+    
+    @Test
+    public void convert_SO_Ok(@TempDir Path tempDir) throws IllegalArgumentException, IoxException, Ili2cException {    
+        Path file = tempDir.resolve("ch_254900.itf");
+        
+        Av2ch av2ch = new Av2ch();
+        av2ch.convert("src/test/data/254900.itf", file.toFile().getParent(), "de");
+        
+        long resultSize = file.toFile().length();
+        assertTrue(resultSize > 580000, "Size of result file is wrong.");
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_ILIDIRS, Validator.SETTING_DEFAULT_ILIDIRS);
+        boolean valid = Validator.runValidation(file.toFile().getAbsolutePath(), settings);
+        assertTrue(valid, "Result file is not valid (ilivalidator).");
     }
 }
